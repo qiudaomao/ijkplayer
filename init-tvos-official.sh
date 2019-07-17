7b7d0ebda243fef9fe26a06803ff206daf5c4a86
qiudaomao <qiudaomao@gmail.com>
2019-07-17 11:16:09 +0800

    change tvos ffmpeg tag

--- init-tvos-official.sh
+++ init-tvos-official.sh
@@ -19,7 +19,7 @@
 IJK_FFMPEG_UPSTREAM=https://github.com/qiudaomao/ffmpeg-ijk-mpv.git
 #IJK_FFMPEG_UPSTREAM=https://github.com/FFmpeg/FFmpeg.git
 IJK_FFMPEG_FORK=https://github.com/qiudaomao/ffmpeg-ijk-mpv.git
-IJK_FFMPEG_COMMIT=2acb6789b2a4e6bc7d0c5ce916a23a0a5ea80ca9
+IJK_FFMPEG_COMMIT=ijk_mpv
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
 IJK_GASP_UPSTREAM=https://github.com/Bilibili/gas-preprocessor.git
